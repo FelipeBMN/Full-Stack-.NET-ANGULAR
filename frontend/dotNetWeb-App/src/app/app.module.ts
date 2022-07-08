7d96fedfdb1d432302fa688781ba4453eabcc49c
FelipeBMN <felipebrunomn@gmail.com>
2022-07-07 23:40:18 -0300

    bootstrap corrigido

--- frontend/dotNetWeb-App/src/app/app.module.ts
+++ frontend/dotNetWeb-App/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from "@angular/common/http"
@@ -6,17 +7,32 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PalestrantesComponent } from './components/palestrantes/palestrantes.component';
 import { EventosComponent } from './components/eventos/eventos.component';
+import { NavComponent } from './components/nav/nav.component';
+
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { CollapseModule } from 'ngx-bootstrap/collapse'
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     PalestrantesComponent,
-    EventosComponent
+    EventosComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BsDropdownModule,
+    TooltipModule,
+    ModalModule,
+    BrowserAnimationsModule,
+    CollapseModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
